6514db11378fa96a571bd99a298296856c1a4b81
Gabriel Ubiale <87863973+gabrielubiale@users.noreply.github.com>
2024-03-20 21:30:47 -0300

    Update products.js

--- src/data/products.js
+++ src/data/products.js
@@ -1,59 +1,57 @@
 const products = [
   {
     id: "6228fe63b7e6cb904bbe0165",
-    price: 150,
-    name: "Suéter Branco",
+    price: 79.99,
+    name: "T-Shirt",
     imageUrl:
-      "https://images.unsplash.com/flagged/photo-1559502867-c406bd78ff24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=685&q=80",
+      "https://img.freepik.com/free-photo/shirt-mockup-concept-with-plain-clothing_23-2149448737.jpg?t=st=1710973270~exp=1710973870~hmac=651225eda3e67f92ad47b4471860c78d68759cc088a83fdcbb342b3485993c3a",
   },
   {
     id: "6228fec7b7e6cb904bbe016f",
-    name: "Regata Azul",
-    price: 150,
-    imageUrl:
-      "https://images.unsplash.com/photo-1469334031218-e382a71b716b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80s",
+    price: 249.99,
+    name: "Black sweater",
+    imageUrl: "https://img.freepik.com/free-photo/man-black-sweater-black-bucket-hat-youth-apparel-shoot_53876-102294.jpg?t=st=1710973404~exp=1710977004~hmac=bd0a9985f5489cc81778e26fab57cd0f828440377dccbeeb02915db020d39295&w=740",
   },
   {
-    price: 200,
     id: "6228ff71b7e6cb904bbe0175",
-    imageUrl:
-      "https://images.unsplash.com/photo-1584670747417-594a9412fba5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-    name: "Suéter Vermelho Vibrante",
+    price: 149.99,
+    name: "Classic shirt",
+    imageUrl: "https://img.freepik.com/free-photo/still-life-with-classic-shirts-hanger_23-2150828581.jpg?t=st=1710973505~exp=1710977105~hmac=39ab3b062b2a7b65ee85405b9dd46795a7a2cff5fb60b9ccde3d9391af9f3c74&w=1380",
   },
   {
-    name: "Vestido Vermelho",
-    price: 250,
-    imageUrl:
-      "https://images.unsplash.com/photo-1572804013309-59a88b7e92f1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=746&q=80",
     id: "6228ff93b7e6cb904bbe0177",
-  },
-  {
-    price: 300,
-    id: "6228fe80b7e6cb904bbe0168",
-    name: "Jaqueta Xadrez",
+    price: 129.99,
+    name: "Fedora hats",
     imageUrl:
-      "https://images.unsplash.com/photo-1525450824786-227cbef70703?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+      "https://img.freepik.com/free-photo/fedora-hats-arrangement-studio_23-2150737111.jpg?t=st=1710973568~exp=1710977168~hmac=f73e105702027ef34a99583a949123ef4f1f9bf426bbc831407a6d0c1e9ec9d0&w=740",
   },
   {
-    price: 200,
-    name: "Blusa Branca",
+    id: "6228fe80b7e6cb904bbe0168",
+    price: 99.99,
+    name: "Polo shirt",
+    imageUrl:
+      "https://img.freepik.com/free-photo/clean-blank-shirt-hanger_23-2149347515.jpg?t=st=1710973632~exp=1710977232~hmac=775bd88b273301f215a334c3d15051309461665c8c16b6645d799601f2ea4e33&w=740",
+  },
+  {
+    price: 189.99,
+    name: "Beige pants",
     id: "6228ff31b7e6cb904bbe0172",
     imageUrl:
-      "https://images.unsplash.com/photo-1624206112918-f140f087f9b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+      "https://img.freepik.com/free-photo/hand-holding-light-brown-beige-pants_23-2150756277.jpg?t=st=1710973714~exp=1710977314~hmac=c1556b000f9d63abcf06b44a5ff300197cb5f817df1eef1a33ad09b6b04b5c74&w=740",
   },
   {
-    price: 200,
+    price: 49.99,
     imageUrl:
-      "https://images.unsplash.com/photo-1582418702059-97ebafb35d09?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80",
+      "https://img.freepik.com/free-photo/winter-holidays-handsome-adult-man-with-red-scarf-looking-happy-camera-standing-sweater-against-white-background_1258-67445.jpg?t=st=1710973877~exp=1710977477~hmac=4e9b94cbf4fa7465e098e53e0d224e7722476d483d85eaaafda15864ab6d21dc&w=1380",
     id: "62290014b7e6cb904bbe017c",
-    name: "Calça Jeans Azul",
+    name: "Red scarf",
   },
   {
     id: "6228ffa5b7e6cb904bbe017a",
-    name: "Vestido Branco",
-    price: 250,
+    name: "Classic Shoes",
+    price: 349.99,
     imageUrl:
-      "https://images.unsplash.com/photo-1515372039744-b8f02a3ae446?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80",
+      "https://img.freepik.com/free-photo/shoes_1303-3608.jpg?t=st=1710974016~exp=1710977616~hmac=124d2b45f3ca4bdbbb890157a13c24a711f2e547fbda771c992ee2a7f92cbc48&w=1380",
   },
 ];
 
